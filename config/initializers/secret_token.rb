bddc5c2abfa157540de2cfea1f8df96d19b6ac69
mrc2407 <mrc2407@gmail.com>
2011-08-05 22:21:20 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KindleLinks::Application.config.secret_token = '11b2eb4d4234136bd7ac3d1498bab0499b16fb1693f2f46180d234fd42cf0b41cd05cb58a5f6f34b98094918582c4a00283f0c58acd9d83b61eebdbc77fb20e8'
