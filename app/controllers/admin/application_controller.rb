8546eb0bcd0b319c3a02e4588ac442d7394957f3
mrc2407 <mrc2407@gmail.com>
2011-08-05 23:04:16 +0200

    Let only admins visit admin namespace

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,11 @@
 class Admin::ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_admin!
+
+  def authenticate_admin!
+    unless current_user && current_user.admin?
+      flash[:error] = t('authentication.users.not_allowed')
+      redirect_to :back
+    end
+  end
 end
