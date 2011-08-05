0c300d28ec20f4236a8892433ad71a66ff2afd91
mrc2407 <mrc2407@gmail.com>
2011-08-05 23:07:10 +0200

    Add admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110805205051) do
+ActiveRecord::Schema.define(:version => 20110805210624) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20110805205051) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
