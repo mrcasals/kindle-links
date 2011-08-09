3ba1e672a725beb488a2febf4d08f81d20641922
Marc Riera <mrc2407@gmail.com>
2011-08-09 17:05:37 +0200

    Fix method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < Devise::SessionsController
   layout 'login'
   def after_sign_in_path_for(model)
-    if current_user.is_admin?
+    if current_user.admin?
       admin_path
     else
       root_path
